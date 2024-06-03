4f2660334cdeb94fd39db5eafad9767d2b10c8a2
Carlo Ferrigno <ferrigno.carlo@gmail.com>
2024-06-03 16:51:00 +0200

    Add init source in xstartup.sh

--- datalabs/xstartup.sh
+++ datalabs/xstartup.sh
@@ -1,6 +1,8 @@
 # Datalabs API was emitting state change before the VNC server was actually ready, 
 # So we moved the websockify to xstartup, to ensure the VNC server is ready.
 # Optimally we would use a VNC server with builtin socket support.
+export HOME_OVERRRIDE=/media/$USER
+source /init.sh
 websockify $IF_main_port localhost:5900 &
 openbox &
 bash -c "sleep 3000"
